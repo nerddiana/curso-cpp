158faf60b79e44686cb8844a50d97090b1bd0ad0
Diana Martínez <diananerdoficial@gmail.com>
2021-05-11 23:37:52 -0500

    Hola mundo

---
+++ 07-hola_mundo/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std; // Importa todo el espacio de nombres
+
+// Esto es un comentario de una línea
+
+/*
+	Esto es un comentario
+	de múltiples líneas
+	...
+*/
+
+int main() {
+	// std::cout << "Hola mundo" << std::endl;
+	cout << "Hola mundo" << endl;
+	return 0;
+}
